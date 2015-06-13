8fcfdd052e483e660344ed7bc2382a9e686ea489
netbe <francois.benaiteau@gmail.com>
2015-06-13 23:00:01 +0200

    new architecture with watch

---
+++ uQu WatchKit Extension/InterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  InterfaceController.h
+//  uQu WatchKit Extension
+//
+//  Created by François Benaiteau on 13/06/15.
+//  Copyright (c) 2015 Cookie Monster. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface InterfaceController : WKInterfaceController
+
+@end
---
+++ uQuKit/uQuKit.h
@@ -0,0 +1,19 @@
+//
+//  uQuKit.h
+//  uQuKit
+//
+//  Created by François Benaiteau on 13/06/15.
+//  Copyright (c) 2015 Cookie Monster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for uQuKit.
+FOUNDATION_EXPORT double uQuKitVersionNumber;
+
+//! Project version string for uQuKit.
+FOUNDATION_EXPORT const unsigned char uQuKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <uQuKit/PublicHeader.h>
+
+
