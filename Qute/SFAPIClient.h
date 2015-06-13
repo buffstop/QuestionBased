399c4a86527aa5f317861edfc26dcf390ed4101c
Andreas Buff <andreas.buff@cgm.com>
2015-06-13 15:18:17 +0200

    adds APIClient skeleton

---
+++ Qute/SFAPIClient.h
@@ -0,0 +1,15 @@
+//
+//  SFAPIClient.h
+//  Qute
+//
+//  Created by Andreas Buff on 13/06/15.
+//  Copyright (c) 2015 Cockie Monster Team. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import <SalesforceRestAPI/SFRestAPI.h>
+
+@interface SFAPIClient : NSObject <SFRestDelegate>
+
+@end
---
+++ Qute/SFAPIClient.m
@@ -0,0 +1,115 @@
+//
+//  SFAPIClient.m
+//  Qute
+//
+//  Created by Andreas Buff on 13/06/15.
+//  Copyright (c) 2015 Cockie Monster Team. All rights reserved.
+//
+
+#import "SFAPIClient.h"
+
+#import <SalesforceSDKCore/SFJsonUtils.h>
+#import <SalesforceRestAPI/SFRestAPI.h>
+#import <SalesforceRestAPI/SFRestRequest.h>
+#import <SalesforceSDKCore/SFSecurityLockout.h>
+#import <SalesforceSDKCore/SFAuthenticationManager.h>
+#import <SalesforceSDKCore/SFDefaultUserManagementViewController.h>
+
+@interface SFAPIClient ()
+@property (strong, nonatomic) NSMutableDictionary *successHandlerForRequest;
+@property (strong, nonatomic) NSMutableDictionary *errorHandlerForRequest;
+@end
+
+@implementation SFAPIClient
+
+- (void)testGet
+{
+    NSDictionary *params = @{@"q":@"select id, name from Answer__c"};
+    [self GETQueryWithParams:params onSuccess:^{
+        NSLog(@"Success");
+    } onError:^(NSError *error) {
+        NSLog(@"Error: %@", error);
+    }];
+}
+
+- (void)GETQueryWithParams:(NSDictionary *)params
+                 onSuccess:(void(^)())successBlock
+                   onError:(void(^)(NSError *error))errorBlock;
+{
+    [self sendQueryWithSFRestMethod:SFRestMethodGET params:params onSuccess:successBlock onError:errorBlock];
+}
+
+- (void)sendQueryWithSFRestMethod:(SFRestMethod)restMethod
+                           params:(NSDictionary *)params
+                        onSuccess:(void(^)())successBlock
+                          onError:(void(^)(NSError *error))errorBlock;
+{
+    NSString *path = @"/v33.0/query";
+    
+    
+    NSDictionary *queryParams = params;
+//    NSDictionary *queryParams = ([params length] == 0
+//                                 ? nil
+//                                 : (NSDictionary *)[SFJsonUtils objectFromJSONString:params]
+//                                 );
+    
+    SFRestRequest *request = [SFRestRequest requestWithMethod:restMethod path:path queryParams:queryParams];
+    
+    self.successHandlerForRequest[request] = successBlock;
+    self.errorHandlerForRequest[request] = errorBlock;
+    
+    [[SFRestAPI sharedInstance] send:request delegate:self];
+}
+
+#pragma mark - Other
+
+- (void)removeHandlerForRequest:(SFRestRequest *)request
+{
+    [self.successHandlerForRequest removeObjectForKey:request];
+    [self.errorHandlerForRequest removeObjectForKey:request];
+}
+
+#pragma mark - SFRestDelegate
+
+- (void)request:(SFRestRequest *)request didLoadResponse:(id)dataResponse {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        void (^successHandler)() = self.successHandlerForRequest[request];
+        [self removeHandlerForRequest:request];
+        if (successHandler) {
+            successHandler();
+        }
+    });
+}
+
+- (void)request:(SFRestRequest*)request didFailLoadWithError:(NSError*)error {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        void (^errorHandler)() = self.errorHandlerForRequest[request];
+        [self removeHandlerForRequest:request];
+        if (errorHandler) {
+            errorHandler();
+        }
+    });
+}
+
+- (void)requestDidCancelLoad:(SFRestRequest *)request {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        void (^errorHandler)() = self.errorHandlerForRequest[request];
+        [self removeHandlerForRequest:request];
+        if (errorHandler) {
+            errorHandler();
+        }
+    });
+}
+
+- (void)requestDidTimeout:(SFRestRequest *)request {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        void (^errorHandler)() = self.errorHandlerForRequest[request];
+        [self removeHandlerForRequest:request];
+        if (errorHandler) {
+            errorHandler();
+        }
+    });
+}
+
+
+@end
